3692c42bda57bb59e21457ca29593b50b516d04d
@jbrzozowski <jeremybrzozowski@gmail.com>
2020-08-30 12:50:06 -0700

    Updating main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,29 +2,7 @@ import os
 import csv
 
 
-# def analyze_fin_data(findata):
-#     financials = list(findata)
-#     print(len(financials))
-    
 
-    
-        
-
-
-
-# * The total number of months included in the dataset
-
-#   * The net total amount of "Profit/Losses" over the entire period
-
-#   * The average of the changes in "Profit/Losses" over the entire period
-
-#   * The greatest increase in profits (date and amount) over the entire period
-
-#   * The greatest decrease in losses (date and amount) over the entire period
-
-# * As an example, your analysis should look similar to the one below:
-
-#   ```text
 #   Financial Analysis
 #   ----------------------------
 #   Total Months: 86
@@ -37,7 +15,7 @@ import csv
 
 # get the path to the csv file
 csvpath = os.path.join("Resources", "budget_data.csv")
-# csvpath = "C:/Gitlab/python-challenge/PyBank/Resources/budget_data.csv"
+
 
 
 # # read the csv file and store in a variable
@@ -47,41 +25,88 @@ with open(csvpath) as csvfile:
     csv_header = next(findata)
 
     financials = list(findata)
-    print(len(financials))
-    # print(financials[0][1])
 
-    pnl = []
+# print(financials)
 
-    for num in financials:
-        pnl.append(int(num[1]))
+upper_range = len(financials)
+max_pnl_increase = 0
+max_pnl_decrease = 0
+total_months = 0
+total_pnl = 0
+pnl_change_total = 0
+pnl_change_count = 0
+pnl_change = 0
+pnl_change_lst = []
+j = 0
+print(financials)
+for i in range(0,upper_range):
+    pnl_change_2 = int(financials[i][1])
+    pnl_change_1 = int(financials[i-1][1])
+    pnl_change = pnl_change_2 - pnl_change_1
+    total_months +=1
+    total_pnl += int(financials[i][1])
 
-    total_pnl = sum(pnl)
-    print(total_pnl)
+    pnl_change_lst.append(pnl_change)
 
-    monthly_change = []
-    upper_range = len(financials)
-    for i in range(1,upper_range):
-        pnl_change_2 = int(financials[i][1])
-        pnl_change_1 = int(financials[i-1][1])
-        pnl_change = pnl_change_2 - pnl_change_1
-        monthly_change.append(int(pnl_change))
+    if pnl_change > max_pnl_increase:
+        max_pnl_increase = pnl_change
+        max_pnl_date = str(financials[i][0])
 
-    # print(monthly_change)
+    if pnl_change < max_pnl_decrease:
+        max_pnl_decrease = pnl_change
+        min_pnl_date = str(financials[i][0])
+    
+print(pnl_change_lst)
+total_pnl_change = sum(pnl_change_lst)
+total_pnl_change_count = len(pnl_change_lst)
+avg_change = total_pnl_change / total_pnl_change_count
 
-    avg_change = sum(monthly_change) / len(monthly_change)
-    max_change = max(monthly_change)
-    max_decrease = min(monthly_change)
+print(str(total_months))
+print(str(total_pnl))
+print(str(avg_change))
+print(max_pnl_date, str(max_pnl_increase))
+print(min_pnl_date, str(max_pnl_decrease))
 
-    print(avg_change)
-    print(max_change)
-    print(max_decrease)
 
+
+#create dictionary for pnl calculations
+monthly_change_date = []
+monthly_change_pnl = []
+
+
+upper_range = len(financials)
+for i in range(1,upper_range):
+    pnl_date = str(financials[i][0])
+    monthly_change_date.append(pnl_date)        
     
 
-    # finstats = []
+for i in range(1,upper_range): 
+    pnl_change_2 = int(financials[i][1])
+    pnl_change_1 = int(financials[i-1][1])
+    pnl_change = pnl_change_2 - pnl_change_1
+    monthly_change_pnl.append(int(pnl_change))
 
-    # for num in financials:
-    #     print(num[1])
+
+stats_dict = dict(zip(monthly_change_date, monthly_change_pnl))
+
+total_pnl_values = 0
+num_values = 0
+max_value = 0
+
+for value in stats_dict.values():
+    total_pnl_values += value
+    num_values += 1
+
+    if value > max_value:
+        max_value = value
+        
+
+
+avg_change = total_pnl_values / num_values
+
+
+# print("Dictionary: " + str(avg_change))
+
 
 
 
