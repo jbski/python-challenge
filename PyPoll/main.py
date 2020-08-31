1fec802cbb719e231cab7d8face6e4569df03e0a
@jbrzozowski <jeremybrzozowski@gmail.com>
2020-08-31 00:50:20 -0700

    Load finished PyPoll files

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,30 +2,6 @@ import os
 import csv
 
 
-# * The total number of votes cast
-
-#   * A complete list of candidates who received votes
-
-#   * The percentage of votes each candidate won
-
-#   * The total number of votes each candidate won
-
-#   * The winner of the election based on popular vote.
-
-# * As an example, your analysis should look similar to the one below:
-
-#   ```text
-#   Election Results
-#   -------------------------
-#   Total Votes: 3521001
-#   -------------------------
-#   Khan: 63.000% (2218231)
-#   Correy: 20.000% (704200)
-#   Li: 14.000% (492940)
-#   O'Tooley: 3.000% (105630)
-#   -------------------------
-#   Winner: Khan
-#   -------------------------
 
 # Get the path to the csv file
 csvpath = os.path.join("Resources", "election_data.csv")
@@ -44,10 +20,14 @@ with open(csvpath) as election_datafile:
     # print(election_header)
     # print(election_data[0])
 
-    # Create an empty list to hold the unique candidate names and related values
-    candidate_info = []
+    # Create an empty list to hold the unique candidate names and total names
+    candidate_names = []
+    names = []
+    names_dct = []
+    vote_pct = []
+    vote_per_candidate = []
 
-    #Get the number of records in the election data
+    #Get the number of records in the election data 
     upper_range = len(election_data)
     
     #Define and initialize variables
@@ -56,21 +36,61 @@ with open(csvpath) as election_datafile:
     #Loop through the election data and get variable values
     for i in range(upper_range):
 
+        #Count the total votes
         total_votes +=1
 
-        if election_data[i][2] not in candidate_info:
-            candidate_info.append(election_data[i][2])
+        #Create a candidate name list for counting purposes
+        names.append(election_data[i][2])
 
-        
-    print(f'Election Results')
+        #Create a list to keep track of unique candidate info
+        if election_data[i][2] not in candidate_names:
+            candidate_names.append(election_data[i][2])
+
+    total_candidates = len(candidate_names)
+
+    #Create a list containing the values for candidate, vote percentage, and total votes per candidate
+    for j in range(total_candidates):
+        names_dct.append(candidate_names[j])
+        vote_pct.append((names.count(candidate_names[j]) / total_votes) * 100)
+        vote_per_candidate.append(names.count(candidate_names[j]))
+
+    #Create a dictionary to help get the winner
+    election_dict = dict(zip(names_dct, vote_pct))
+    
+    winner = max(election_dict, key=election_dict.get)
+    
+    #Print the results to the terminal
+    print(f'\nElection Results')
     print(f'-----------------------------------')
-    print(f'Total Votes {total_votes}')
+    print(f'Total Votes: {total_votes}')
+    print(f'-----------------------------------')
+                
+    for j in range(total_candidates):
+        print(f'{candidate_names[j]}: {(names.count(candidate_names[j]) / total_votes) * 100:.3}%  ({names.count(candidate_names[j])})')
+
+    print(f'-----------------------------------')
+    print(f'Winner: {winner}')
+    print(f'-----------------------------------\n')
+
+    #Write the results to an output file
+    csv_save_path = os.path.join("analysis", "election_data_output.csv")
+
+    analysis_results_output = open(csv_save_path, mode = "w", newline="")
+
+    csv_writer = csv.writer(analysis_results_output, delimiter=',')
+
+    csv_writer.writerow(["Election Results"])
+    csv_writer.writerow(["-----------------------------------"])
+    csv_writer.writerow([f'Total Votes: {total_votes}'])
+    csv_writer.writerow(["-----------------------------------"])
+    for j in range(total_candidates):
+        csv_writer.writerow([f'{candidate_names[j]}: {(names.count(candidate_names[j]) / total_votes) * 100:.3}%  ({names.count(candidate_names[j])})'])
+
+    csv_writer.writerow(["-----------------------------------"])
+
+    csv_writer.writerow([f'Winner: {winner}'])
+    csv_writer.writerow(["-----------------------------------"])
     
-    total_candidates = len(candidate_info)
-    for i in range(total_candidates):
-        print(f'{candidate_info[i]} :')
-
-    # print(candidate_info)
     
 
 
@@ -79,6 +99,3 @@ with open(csvpath) as election_datafile:
 
 
 
-
-
-# Read the csv
